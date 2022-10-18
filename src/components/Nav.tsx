dc5f9fc0b7ece538fa4ac8695f7a3161e95be89c
Gui-dev <adrianon10@gmail.com>
2022-10-18 19:16:03 -0300

    feat: add component Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,22 @@
+import Link from 'next/link'
+
 export const Nav = () => {
   return (
-    <h1>Nav</h1>
+    <nav className="flex items-center justify-between py-10">
+      <Link href="/">
+        <button className="text-lg text-white font-medium">
+          Creative Minds
+        </button>
+      </Link>
+      <ul className="flex items-center gap-10">
+        <li>
+          <Link href="/auth/login">
+            <a className="text-sm text-zinc-800 font-medium ml-8 py-2 px-4 bg-green-500 rounded-lg hover:bg-green-400">
+              Join Now
+            </a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
