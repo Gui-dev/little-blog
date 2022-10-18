d7dd8998676a97e03428df3537da1a67f3c1398f
Gui-dev <adrianon10@gmail.com>
2022-10-18 19:16:18 -0300

    feat: add component Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { ReactNode } from 'react'
 import { Nav } from './Nav'
 
@@ -7,9 +8,12 @@ type LayoutProps = {
 
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <>
+    <div className="mx-6 md-mx-auto md:max-w-2xl">
+      <Head>
+        <title>Creative Minds</title>
+      </Head>
       <Nav />
       <main>{children}</main>
-    </>
+    </div>
   )
 }
