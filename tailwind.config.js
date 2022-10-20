7e4d8ab7257a91f86a1ed5f74f63d2d705a2be86
Gui-dev <adrianon10@gmail.com>
2022-10-20 04:02:15 -0300

    chore: add gradient-text

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
     extend: {},
     fontFamily: {
       sans: ['Poppins', 'sans-serif']
+    },
+    backgroundImage: {
+      'gradient-text': 'linear-gradient(89.86deg, rgb(34 197 94) 13.08%, rgb(34 211 238) 60.94%, rgb(249 168 212) 10.57%)'
     }
   },
   plugins: []
