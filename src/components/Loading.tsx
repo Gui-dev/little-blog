05a1e0a6f4484d7459a4a374e9149d0120c16fa2
Gui-dev <adrianon10@gmail.com>
2022-10-20 04:03:24 -0300

    feat: add component Loading

---
+++ src/components/Loading.tsx
@@ -0,0 +1,12 @@
+import { Spinner } from 'phosphor-react'
+
+const Loading = () => {
+  return (
+    <Spinner
+      weight="bold"
+      className="text-2xl text-pink-300 animate-spin"
+    />
+  )
+}
+
+export default Loading
