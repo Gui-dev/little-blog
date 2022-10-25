667bd7cb13a491f77fdab65835667c4e1e9cc5dc
Gui-dev <adrianon10@gmail.com>
2022-10-25 03:41:07 -0300

    feat: add ToastifyContainer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ToastContainer } from 'react-toastify'
 
 import { Layout } from '../components/Layout'
+import 'react-toastify/dist/ReactToastify.css'
 
 function MyApp ({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <ToastContainer position="top-center" limit={1} theme="dark" />
       <Component {...pageProps} />
     </Layout>
   )
